58aace1923ab65bb77f9a317b540f57e4a5f1690
Maylinda Christy <98601730+maylindachristy@users.noreply.github.com>
2024-06-25 23:39:05 +0700

    Add files via upload

---
+++ user/navigasiuser.php
@@ -0,0 +1,28 @@
+<div id="nav-wrapper">
+    <div class="header_nav">Menu Navigasi</div>
+    <ul class="nav" id="accordion">
+        <li><a href="jadwal.php">Home</a></li>
+        <li class="accordion-toggle" id="konsumen">Konsumen</li>
+        <div class="accordion-content default">
+            <li><a href="userkonsumen.php">Lihat Data Konsumen</a></li>
+        </div>
+        <li class="accordion-toggle">Pegawai</li>
+        <div class="accordion-content default">
+            <li><a href="userpegawai.php">Lihat Data Pegawai</a></li>
+        </div>
+        <li class="accordion-toggle">Tujuan</li>
+        <div class="accordion-content default">
+            <li><a href="usertujuan.php">Lihat Data Tujuan</a></li>
+        </div>
+        <li class="accordion-toggle">Tiket</li>
+        <div class="accordion-content default">
+            <li><a href="usertiket.php">Lihat Data Tiket</a></li>
+        </div>
+        <li class="accordion-toggle">Pembayaran</li>
+        <div class="accordion-content default">
+            <li><a href="userpembayaran.php">Lihat Data Pembayaran</a></li>
+        </div>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+    <p>Tiket Online V1.0</p>
+</div>
